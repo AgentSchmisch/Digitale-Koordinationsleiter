a735a8d3ead50f8f53720d902714f0cdc2412bd2
Florian Schmid <78489341+AgentSchmisch@users.noreply.github.com>
2021-08-28 17:26:55 +0200

    Adding KinectCoordinates_Test1

---
+++ KinectCoordinates_Test1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace KinectCoordinates_Test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblX = new System.Windows.Forms.TextBox();
+            this.lblY = new System.Windows.Forms.TextBox();
+            this.lblZ = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblX
+            // 
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX.Location = new System.Drawing.Point(210, 151);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(138, 60);
+            this.lblX.TabIndex = 0;
+            // 
+            // lblY
+            // 
+            this.lblY.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblY.Location = new System.Drawing.Point(210, 217);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(138, 60);
+            this.lblY.TabIndex = 1;
+            // 
+            // lblZ
+            // 
+            this.lblZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZ.Location = new System.Drawing.Point(210, 283);
+            this.lblZ.Name = "lblZ";
+            this.lblZ.Size = new System.Drawing.Size(138, 60);
+            this.lblZ.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 54);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 443);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 60);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 377);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 60);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 54);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 515);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblZ);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox lblX;
+        private System.Windows.Forms.TextBox lblY;
+        private System.Windows.Forms.TextBox lblZ;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ KinectCoordinates_Test1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KinectCoordinates_Test1
+{
+    public partial class Form1 : Form
+    {
+        KinectSensor kinectSensor = null;
+        BodyFrameReader bodyFrameReader = null;
+        Body[] bodies = null;
+
+
+            public Form1()
+        {
+            InitializeComponent();
+            InitializeKinect();
+
+        }
+        public void InitializeKinect()
+        {
+            kinectSensor = KinectSensor.GetDefault();
+            if (kinectSensor != null) //turn on kinect
+            {
+                kinectSensor.Open();
+                label2.Text = "Kinect gefunden";
+            }
+            else
+                label2.Text = "Kinect nicht gefunden";
+            {
+                bodyFrameReader = kinectSensor.BodyFrameSource.OpenReader();
+                if (bodyFrameReader!=null)
+
+                {
+                    bodyFrameReader.FrameArrived += reader_FrameArrived;
+                }
+            }
+        }
+
+        private void reader_FrameArrived(object sender,BodyFrameArrivedEventArgs e)
+        {
+            bool dataRecieved = false;
+            using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame!=null)
+                {
+                    if (bodies == null)
+                    {
+                        bodies = new Body[bodyFrame.BodyCount];
+                    }
+                    bodyFrame.GetAndRefreshBodyData(bodies);
+
+
+                }
+                
+                dataRecieved = true;
+            }
+            if (dataRecieved)
+            {
+                foreach (Body body in bodies)
+                {
+                    if (body.IsTracked)
+                    {
+                        IReadOnlyDictionary<JointType, Joint> joints = body.Joints;
+                        Dictionary<JointType, Point> jointPoints = new Dictionary<JointType, Point>();
+
+                        Joint HandRight = joints[JointType.HandRight];
+                        
+                        float rh_distance_x = HandRight.Position.X;
+                        float rh_distance_y = HandRight.Position.Y;
+                        float rh_distance_z = HandRight.Position.Z;
+
+                        lblX.Text = rh_distance_x.ToString("#.##");
+                        lblY.Text = rh_distance_y.ToString("#.##");
+                        lblZ.Text = rh_distance_z.ToString("#.##");
+                        //valuechanged(rh_distance_x, rh_distance_y, rh_distance_z);
+                        label1.Text = body.HandRightState.ToString();
+
+                    }
+
+                }
+            }
+        }
+
+        //test der wertermittlung und durchschnittsberechnung
+        public double[,] valuechanged(double x,double y,double z)
+        {
+            double[,] values = new double[30, 3]; //array mit 
+            for (int i = 0; i < 31; i++)
+            {
+                for (int j= 0; j < 3; j++)
+                {
+                    if (j==1)
+                    {
+                        values[i, j] += x;
+                    }
+                    if (j == 2)
+                    {
+                        values[i, j] += y;
+                    }
+                    if (j == 3)
+                    {
+                        values[i, j] += z;
+                    }
+                    double val_x = values[0, 0] / 30 ;
+                    lblX.Text = val_x.ToString();
+                    double val_y = values[0, 0] / 30;
+                    lblX.Text = val_y.ToString();
+                    double val_z = values[0, 0] / 30;
+                    lblX.Text = val_z.ToString();
+                }
+                
+            }
+            return values;
+        }
+
+
+    }
+}
