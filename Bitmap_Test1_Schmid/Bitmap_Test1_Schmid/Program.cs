33f0debfb8481dddf87d852a8844d0ba06e735ef
Christoph Polke <christophpolke86@gmail.com>
2022-02-26 13:25:45 +0100

    update_v2_analyse

--- Bitmap_Test1_Schmid/Bitmap_Test1_Schmid/Program.cs
+++ Bitmap_Test1_Schmid/Bitmap_Test1_Schmid/Program.cs
@@ -15,8 +15,8 @@ namespace Bitmap_Test1_Schmid
             Application.SetCompatibleTextRenderingDefault(false);
             try
             {
-                //Application.Run(new Form1());
-                Application.Run(new Analyse());
+                Application.Run(new Form1());
+               
             }
             catch
             {
